a64e4649b68e7713ab4a98bfcb6b4c2c588b11f1
tmortx7 <99x7eo@gmail.com>
2023-02-19 11:05:38 -0700

    nav

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,12 +1,14 @@
 import { Navbar } from "./Navbar";
 
-const MainLayout = ({children}:{children:React.ReactNode}) => {
-  return(
-    <div className="flex flex-col w-full h-[100vh]">
-      <Navbar />
-      {children}
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex w-full flex-row justify-center">
+      <div className="flex h-[100vh] w-full max-w-7xl flex-col ">
+        <Navbar />
+        {children}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default MainLayout;
